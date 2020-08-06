8e7e2fc2bd3f51b3bc76e6541a5c127855a8536a
erivelto47 <erivelto02@hotmail.com>
2020-08-06 00:55:06 -0300

    Add styles and components

--- web/src/pages/teacher-form/index.tsx
+++ web/src/pages/teacher-form/index.tsx
@@ -1,11 +1,59 @@
 import React from 'react'
+
 import PageHeader from "../../components/page-header";
+import Input from "../../components/input";
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './style.css'
+import TextArea from "../../components/text-area";
+import Select from "../../components/select";
+
 function TeacherForm() {
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrivel que voce quer dar aulas."/>
+      <PageHeader
+        title="Que incrivel que voce quer dar aulas."
+        description="O primeiro Passo e preencher esse formulario de inscricao"
+      />
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input label="Nome completo" name="name"/>
+          <Input label="Avatar" name="avatar"/>
+          <Input label="whatsapp" name="whatsapp"/>
+          <TextArea label="Biografia" name="bio"/>
+        </fieldset>
+
+        <fieldset>
+          <legend>Sobre a Aula</legend>
+          <Select
+            label="Materia"
+            name="subject"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Ciencias', label: 'Ciencias'},
+              {value: 'Fisica', label: 'Fisica'},
+              {value: 'Matematica', label: 'Matematica'}
+            ]}/>
+          <Input
+            label="Custo da sua hora por aula"
+            name="cost"
+            type="number"/>
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Aviso importante"/>
+            Importante! <br/>
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
     </div>
   )
 }
--- web/src/pages/teacher-list/index.tsx
+++ web/src/pages/teacher-list/index.tsx
@@ -1,29 +1,39 @@
 import React from 'react'
 
 import PageHeader from "../../components/page-header";
-import avatarImg from '../../assets/images/avatar.png'
-import wppIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './style.css'
 import TeacherItem from "../../components/teacher-item";
+import Input from "../../components/input";
+import Select from "../../components/select";
 
 function TeacherList () {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes sao os Professores disponiveis">
         <form id="search-teachers">
-          <div className="input-block">
-            <label htmlFor="subject">Materia</label>
-            <input type="text" id="subject"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="day-week">Dia da seman</label>
-            <input type="text" id="day-week"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="hour">Hora</label>
-            <input type="text" id="hour"/>
-          </div>
+          <Select
+            label="Materia"
+            name="subject"
+            options={[
+              {value: 'Artes', label: 'Artes'},
+              {value: 'Biologia', label: 'Biologia'},
+              {value: 'Ciencias', label: 'Ciencias'},
+              {value: 'Fisica', label: 'Fisica'},
+              {value: 'Matematica', label: 'Matematica'}
+            ]}/>
+          <Select
+            label="Dia da Semana"
+            name="week_day"
+            options={[
+              {value: '0', label: 'Segunda-feira'},
+              {value: '1', label: 'Terca-feira'},
+              {value: '2', label: 'Quarta-feira'},
+              {value: '3', label: 'Quinta-feira'},
+              {value: '4', label: 'Sexta-feira'}
+            ]}/>
+          <Input label="Dia da seman" name="day-week"/>
+          <Input label="Hora" name="hour" type="time"/>
         </form>
       </PageHeader>
 
