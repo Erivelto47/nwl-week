c72ef8093a68ba88a472221241836047526728d7
erivelto47 <erivelto02@hotmail.com>
2020-08-05 23:00:25 -0300

    Creating routes and controllers

--- server/src/App.ts
+++ server/src/App.ts
@@ -1,8 +1,8 @@
 import express from 'express'
-// import cors from 'cors'
+import cors from 'cors'
 // import { createConnection } from 'typeorm'
 
-// import routes from './routes'
+import routes from './routes'
 
 class App {
   public express: express.Application
@@ -11,23 +11,16 @@ class App {
     this.express = express()
 
     this.middlewares()
-    App.connection()
     this.routes()
   }
 
   private middlewares(): void {
     this.express.use(express.json())
-    // this.express.use(cors())
-  }
-
-  private static connection(): void {
-    // createConnection()
-    //   .then(() => console.log('connected'))
-    //   .catch(() => console.log('error to connect'))
+    this.express.use(cors())
   }
 
   private routes(): void {
-    // this.express.use(routes)
+    this.express.use(routes)
   }
 }
 
