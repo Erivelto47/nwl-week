513c51357ebeb3602db5729807b6beae13f997f3
Erivelto Muller <erivelto02@hotmail.com>
2020-10-18 17:51:26 -0300

    - Create routes and pages - Create backend project - Create Bd and connection - Create migrations of bd

--- nlw3/web/src/App.tsx
+++ nlw3/web/src/App.tsx
@@ -1,22 +1,13 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi'
+
+import Routes from './routes';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
-
-import logoImg from './images/landing.svg'
 
 function App() {
   return (
-    <div id="page-landing">
-        <div className="content-wrapper">
-        <img src={logoImg} alt="Happy"/>
-
-        <main>
-            <h1>Leve</h1>
-            <p>Visite</p>
-        </main>
-        </div>
-    </div>
+      <Routes />
   );
 }
 
